7fc7871a9feb1c6d44c0349ae3b1b745c417f0a9
leo <69598134+Leeeeeeeeeeeeeo@users.noreply.github.com>
2020-10-26 15:28:33 -0400

    building buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,4 +3,18 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
+  let buttonClass = "button";
+
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  } 
+  if (props.danger) {
+    buttonClass += " button--danger";
+  }
+ 
+
+  return <button className={buttonClass}>{props.children}
+  </button>;
+
 }
+
