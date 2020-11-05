45e374143d1e6038c01a8ee7c05db01922895cb3
leo <69598134+Leeeeeeeeeeeeeo@users.noreply.github.com>
2020-11-04 21:16:59 -0500

    cypress tests

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -13,41 +13,14 @@ import { render } from "@testing-library/react";
 /*
   We import the component that we are testing
 */
-import Application from "components/Application";
-import Header from "components/Appointment/Header";
-import Show from "components/Appointment/Show";
-import Empty from "components/Appointment/Empty";
-import Confirm from "components/Appointment/Confirm";
-import Form from "components/Appointment/Form";
-import Status from "components/Appointment/Status";
-import Error from "components/Appointment/Error";
+
+import Appointment from "components/Appointment";
+
 /*
   A test that renders a React Component
 */
-describe("Appointment", () => {
-
-it("renders without crashing", () => {
-  render(<Application />);
+describe('Appointment', () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
 });
-it("renders without crashing", () => {
-  render(<Header />);
-});
-it("renders without crashing", () => {
-  render(<Show />);
-});
-it("renders without crashing", () => {
-  render(<Empty />);
-});
-it("renders without crashing", () => {
-  render(<Confirm />);
-});
-it("renders without crashing", () => {
-  render(<Form />);
-});
-it("renders without crashing", () => {
-  render(<Status />);
-});
-it("renders without crashing", () => {
-  render(<Error />);
-});
-})
